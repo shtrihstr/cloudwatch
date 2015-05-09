453fdb98213462fda2c4d9bcba15db40fb5411dd
Aleksandr Strikha <alex@pingbull.no>
2015-05-09 23:26:10 +0200

    send meta to AutoScalingGroup

--- cloudwatch.py
+++ cloudwatch.py
@@ -1,6 +1,8 @@
 import psutil
+import os
 from boto.ec2 import cloudwatch
 from boto.utils import get_instance_metadata
+import boto.ec2.autoscale
 
 class Metric:
     _metrics = {}
@@ -11,6 +13,12 @@ class Metric:
 
         self._metrics[unit].update({key: value})
 
+    def _get_auto_scaling_group_name(self, instance_id, region):
+        conn = boto.ec2.autoscale.AutoScaleConnection()
+        boto.ec2.autoscale.connect_to_region(region)
+        groups = conn.get_all_autoscaling_instances([instance_id])
+        return groups[0].group_name if len(groups) > 0 else None
+
     def send(self):
         metadata = get_instance_metadata()
         instance_id = metadata['instance-id']
@@ -18,8 +26,12 @@ class Metric:
 
         cw = cloudwatch.connect_to_region(region)
 
+        group = self._get_auto_scaling_group_name(instance_id, region)
+
         for (unit, metrics) in self._metrics.items():
-            cw.put_metric_data('EC2', metrics.keys(), metrics.values(), unit=unit, dimensions={"InstanceId": instance_id})
+            cw.put_metric_data('EC2', metrics.keys(), metrics.values(), unit=unit, dimensions={'InstanceId': instance_id})
+            if group:
+                cw.put_metric_data('EC2', metrics.keys(), metrics.values(), unit=unit, dimensions={'AutoScalingGroupName': group})
 
 if __name__ == '__main__':
 
